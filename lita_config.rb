fef3c2bbe0ebe5e2b8d6ad3eeee325d2922f944a
Max Beizer <max.beizer@gmail.com>
2015-06-05 19:01:30 -0500

    Update for deployment

--- lita_config.rb
+++ lita_config.rb
@@ -1,33 +1,27 @@
+require 'dotenv'
+
+Dotenv.tap do |de|
+  # load contents of file at LITA_ENV, useful for production
+  de.load(ENV['LITA_ENV']) if ENV['LITA_ENV']
+  # load contents of .env from current directory, useful for development
+  de.load
+end
+
 Lita.configure do |config|
-  # The name your robot will use.
-  config.robot.name = 'Sancho'
-
-  # The locale code for the language to use.
-  # config.robot.locale = :en
-
   # The severity of messages to log. Options are:
   # :debug, :info, :warn, :error, :fatal
-  # Messages at the selected level and above will be logged.
-  config.robot.log_level = :info
+  config.robot.log_level = ENV['LOG_LEVEL'].intern
 
-  # An array of user IDs that are considered administrators. These users
-  # the ability to add and remove other users from authorization groups.
-  # What is considered a user ID will change depending on which adapter you use.
-  # config.robot.admins = ["1", "2"]
+  # How you interact, either shell or slack
+  config.robot.adapter = ENV['LITA_ADAPTER'].intern
 
-  # The adapter you want to connect with. Make sure you've added the
-  # appropriate gem to the Gemfile.
-  config.robot.adapter = :shell
+  # Config for slack adapter
+  config.adapters.slack.token = ENV['SLACK_TOKEN']
 
-  ## Example: Set options for the chosen adapter.
-  # config.adapter.username = "myname"
-  # config.adapter.password = "secret"
+  # Config for shell adapter
+  config.robot.name = ENV['LITA_NAME']
 
-  ## Example: Set options for the Redis connection.
-  # config.redis.host = "127.0.0.1"
-  # config.redis.port = 1234
-
-  ## Example: Set configuration for any loaded handlers. See the handler's
-  ## documentation for options.
-  # config.handlers.some_handler.some_config_key = "value"
+  # Redis
+  config.redis.host = ENV['REDIS_URL']
+  config.redis.port = ENV['REDIS_PORT']
 end
